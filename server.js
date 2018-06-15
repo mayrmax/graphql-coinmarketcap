40b8fff52ddd556c78b65345ce5708e66ad1b981
Maximilian Zauner <zaunermax@gmail.com>
2018-06-15 11:21:52 +0200

    refactor(code): refactored code, set ide node assistance

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
-var cors = require('cors')
-var bodyParser = require('body-parser')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
-
 const schema = require('./schema')
 
 app.use('/graphql', cors(), bodyParser.json(), graphqlHTTP({
